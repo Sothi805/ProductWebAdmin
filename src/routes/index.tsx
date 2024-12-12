8929b6d806922d22a4f4ce41dc4c373017c771e8
MONY-007 <you@example.com>
2024-12-12 13:26:11 +0700

    fixing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,29 @@
 import { AnimatePresence } from "framer-motion";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "@/features/home/HomePage";
 import LoginPage from "@/features/auth/page/LoginPage";
 import DriverMasterRoute from "./DriverMasterRoute";
 import { useCookies } from "react-cookie";
 import Dashboard from "./Dashboard";
 
-
-
 const Router = () => {
-  const [cookies, setCookie] = useCookies(["token"]);
-console.log(setCookie);
+  const [cookies] = useCookies(["token"]);
+
   return (
     <AnimatePresence>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          {!cookies.token && <Route path="*" element={<LoginPage />} />}
-          <Route
-            path="/"
-            element={cookies.token ? <HomePage /> : <LoginPage />}
-          >
-            {cookies.token && (
-              <>
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path="/products/*" element={<DriverMasterRoute />} />
-                <Route path="/dashboard/*" element={<Dashboard />} />
-              </>
-            )}
-          </Route>
+          {/* Redirect to /login if no token is present */}
+          {!cookies.token && <Route path="*" element={<Navigate to="/login" />} />}
+          {cookies.token ? (
+            <Route path="/" element={<HomePage />}>
+              <Route path="/products/*" element={<DriverMasterRoute />} />
+              <Route path="/dashboard/*" element={<Dashboard />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          ) : (
+            <Route path="/login" element={<LoginPage />} />
+          )}
         </Routes>
       </BrowserRouter>
     </AnimatePresence>
@@ -41,8 +36,7 @@ const NotFoundPage = () => {
   return (
     <div className="w-full h-full flex justify-center items-center">
       <span className="font-extrabold text-2xl">
-        {" "}
-        404 - Oop Page Not Found !
+        404 - Oops! Page Not Found!
       </span>
     </div>
   );
